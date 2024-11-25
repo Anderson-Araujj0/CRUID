cd1b06a8c5ff76b4067ed4ed2d19605856a8e34c
Anderson Araujo <148157330+Anderson-Araujj0@users.noreply.github.com>
2024-11-25 10:23:13 -0300

    DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.crudapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/Cruid";
+    private static final String USER = "seu_usuario";
+    private static final String PASSWORD = "sua_senha";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
